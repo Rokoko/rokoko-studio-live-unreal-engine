c7df6adbb8ced4cd8763b9703e34ef19fc46885a
Sergei Solokhin <sergei@rokoko.com>
2023-05-03 08:18:55 +0200

    Update RokokoReceiver.cpp

--- Plugins/Smartsuit/Source/Smartsuit/Private/RokokoReceiver.cpp
+++ Plugins/Smartsuit/Source/Smartsuit/Private/RokokoReceiver.cpp
@@ -1,6 +1,6 @@
 // Copyright 2019 Rokoko Electronics. All Rights Reserved.
 
-#include "SmartsuitReceiver.h"
+#include "RokokoReceiver.h"
 #include "Smartsuit.h"
 #include "VirtualProductionSource.h"
 
