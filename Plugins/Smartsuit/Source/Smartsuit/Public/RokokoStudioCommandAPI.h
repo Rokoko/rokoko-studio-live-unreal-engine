1fe1bd8025f35867599b27c90e447f8308734a58
Sergei Solokhin <sergei@rokoko.com>
2023-06-14 11:17:19 +0200

    refactoring and added Info command to command api

--- Plugins/Smartsuit/Source/Smartsuit/Public/RokokoStudioCommandAPI.h
+++ Plugins/Smartsuit/Source/Smartsuit/Public/RokokoStudioCommandAPI.h
@@ -22,7 +22,53 @@ struct FRokokoCommandAPI_IPInfo
 	FString APIKey;
 };
 
+USTRUCT(BlueprintType)
+struct FRokokoCommandAPI_CalibrateInput
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	FString DeviceID;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	int32 CountdownDelay;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	bool ShouldSkipSuit;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	bool ShouldSkipGloves;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	bool UseCustomPose;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	FString CustomPoseName;
+};
+
+USTRUCT(BlueprintType)
+struct FRokokoCommandAPI_TrackerInput
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	FString DeviceID;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	FString BoneName;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	FTransform Transform;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	float TimeoutTime{ 2.f };
+
+	UPROPERTY(BlueprintReadWrite, Category = "Command API")
+	bool ShouldQueryOnly{ false };
+};
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnCompletedRequest, int32, ResponseCode, const FString&, ResponseContentString, bool, bSucceeded);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnInfoRequest, const TArray<FString>&, Devices, const TArray<FString>&, Clips);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnTrackerRequest, FVector, Position, FQuat, Rotation);
 
 /**
@@ -37,23 +83,32 @@ public:
 
 	URokokoStudioCommandAPI();
 
+	UFUNCTION(BlueprintCallable, Category = "Command API")
+	void Info(const FRokokoCommandAPI_IPInfo& IPInfo, bool ShouldIncludeDevices, bool ShouldIncludeClips);
+
 	UFUNCTION(BlueprintCallable, Category="Command API")
 	void Restart(const FRokokoCommandAPI_IPInfo& IPInfo, const FString& SmartSuitName);
 
-	UFUNCTION(BlueprintCallable, Category="Command API")
-	void Calibrate(const FRokokoCommandAPI_IPInfo& IPInfo, const FString& SmartSuitName, int32 CountdownDelay=3);
+	UFUNCTION(BlueprintCallable, Category = "Command API")
+	void ResetActor(const FRokokoCommandAPI_IPInfo& IPInfo, const FString& ActorName);
 
 	UFUNCTION(BlueprintCallable, Category="Command API")
-	void StartRecording(const FRokokoCommandAPI_IPInfo& IPInfo, const FString& FileName);
+	void Calibrate(const FRokokoCommandAPI_IPInfo& IPInfo, const FRokokoCommandAPI_CalibrateInput& Params);
 
 	UFUNCTION(BlueprintCallable, Category="Command API")
-	void StopRecording(const FRokokoCommandAPI_IPInfo& IPInfo);
+	void StartRecording(const FRokokoCommandAPI_IPInfo& IPInfo, const FString FileName, const FTimecode StartTime);
+
+	UFUNCTION(BlueprintCallable, Category="Command API")
+	void StopRecording(const FRokokoCommandAPI_IPInfo& IPInfo, const FTimecode EndTime, bool ShouldBackToLive);
 
 	UFUNCTION(BlueprintCallable, Category = "Command API")
-	void Tracker(const FRokokoCommandAPI_IPInfo& IPInfo, const FString& DeviceId, const FString& BoneName, const FTransform& transform, float timeoutTime=2.f, bool isQueryOnly=false);
+	void Tracker(const FRokokoCommandAPI_IPInfo& IPInfo, const FRokokoCommandAPI_TrackerInput& Params);
 
 	UPROPERTY(BlueprintAssignable, Category="Command API")
 	FOnCompletedRequest OnCompletedRequest;
+
+	UPROPERTY(BlueprintAssignable, Category = "Command API")
+	FOnInfoRequest OnInfoRequest;
 
 	UPROPERTY(BlueprintAssignable, Category = "Command API")
 	FOnTrackerRequest OnTrackerRequest;
@@ -68,5 +123,6 @@ public:
 	void SaveConfigFile(const FRokokoCommandAPI_IPInfo& IPInfo, const FString& SmartSuitname);
 
 	void OnProcessRequestComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+	void OnInfoRequestComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
 	void OnTrackerRequestComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
 };
