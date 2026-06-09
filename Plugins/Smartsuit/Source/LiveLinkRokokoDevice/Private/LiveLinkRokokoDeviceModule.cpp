943e5e4efe97f2e6e13193d0a16ad2a2c0836e90
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2026-06-09 18:03:16 +0300

    Don't autoload the device into llh

--- Plugins/Smartsuit/Source/LiveLinkRokokoDevice/Private/LiveLinkRokokoDeviceModule.cpp
+++ Plugins/Smartsuit/Source/LiveLinkRokokoDevice/Private/LiveLinkRokokoDeviceModule.cpp
@@ -23,13 +23,6 @@ public:
 
         PropertyModule.NotifyCustomizationModuleChanged();
 
-        PostEngineInitHandle = FCoreDelegates::GetOnPostEngineInit().AddRaw(this, &FLiveLinkRokokoDeviceModule::TryAutoRegisterDevice);
-
-        // Handle the case where the module loads after engine startup has already completed.
-        if (GEngine)
-        {
-            TryAutoRegisterDevice();
-        }
     }
 
     virtual void ShutdownModule() override
@@ -48,44 +41,6 @@ public:
             PropertyModule.UnregisterCustomClassLayout(
                 ULiveLinkRokokoDeviceSettings::StaticClass()->GetFName());
         }
-    }
-
-private:
-    void TryAutoRegisterDevice()
-    {
-        if (!GEngine)
-        {
-            return;
-        }
-
-        ULiveLinkDeviceSubsystem* DeviceSubsystem = GEngine->GetEngineSubsystem<ULiveLinkDeviceSubsystem>();
-        if (!DeviceSubsystem)
-        {
-            UE_LOG(LogLiveLinkRokokoDevice, Warning, TEXT("LiveLinkDeviceSubsystem unavailable; skipping auto-registration."));
-            return;
-        }
-
-        TArray<ULiveLinkDevice*> ExistingDevices;
-        DeviceSubsystem->GetDevicesByClass(ULiveLinkRokokoDevice::StaticClass(), ExistingDevices);
-        if (!ExistingDevices.IsEmpty())
-        {
-            UE_LOG(LogLiveLinkRokokoDevice, Verbose, TEXT("Rokoko device already present; skipping auto-registration."));
-            return;
-        }
-
-        ULiveLinkRokokoDeviceSettings* SettingsTemplate = NewObject<ULiveLinkRokokoDeviceSettings>(GetTransientPackage());
-        SettingsTemplate->DisplayName = TEXT("Rokoko Studio Device");
-
-        ULiveLinkDeviceSubsystem::FCreateResult CreateResult =
-            DeviceSubsystem->CreateDeviceOfClass(ULiveLinkRokokoDevice::StaticClass(), SettingsTemplate);
-
-        if (!CreateResult.HasValue())
-        {
-            UE_LOG(LogLiveLinkRokokoDevice, Warning, TEXT("Failed to auto-register default Rokoko Live Link device."));
-            return;
-        }
-
-        UE_LOG(LogLiveLinkRokokoDevice, Log, TEXT("Auto-registered default Rokoko Live Link device."));
     }
 
 private:
--- build_plugin.bat
+++ build_plugin.bat
@@ -1,14 +1,14 @@
 set PLUGINNAME=Smartsuit
-set ENGINEDIR=H:\Program Files\Epic Games\UE_5.2
+set ENGINEDIR=D:\UnrealEngine
 set RUNUAT=%ENGINEDIR%\Engine\Build\BatchFiles\RunUAT.bat
 set package_dir=%~dp0Build\Plugins\%PLUGINNAME%
 set plugin_dir=%~dp0Plugins\%PLUGINNAME%
 
 rmdir %package_dir% /s /q
 
-call "%RUNUAT%" BuildPlugin -Plugin="%plugin_dir%\%PLUGINNAME%.uplugin" -Package=%package_dir% -Rocket
+call "%RUNUAT%" BuildPlugin -Plugin="%plugin_dir%\%PLUGINNAME%.uplugin" -Package=%package_dir% -Rocket -TargetPlatforms=Win64
 
-rmdir %plugin_dir%\Binaries /s /q
+rmdir %plugin_dir%\Binaries  -/s /q
 rmdir %plugin_dir%\Intermediate /s /q
 
 Robocopy "%package_dir%\Binaries" "%plugin_dir%\Binaries" /E
